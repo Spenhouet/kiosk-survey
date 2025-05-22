6c0485d8e38563547cca241378f5542b56bee5d4
Sebastian Penhouet <s.penhouet@outlook.de>
2025-05-22 12:57:39 +0200

    Try fix of base path

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,14 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: { 
+  kit: {
     adapter: adapter({
       fallback: '404.html'
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH : ''
     }
-},
+  },
 };
 
 export default config;
