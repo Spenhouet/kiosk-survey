9eb5db1a45f3b140c42d438ed78ce3d3627763ca
Sebastian Penhouet <s.penhouet@outlook.de>
2025-05-22 12:47:44 +0200

    Fix deployment

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+    import { m } from "$lib/paraglide/messages.js";
+
+	function goHome() {
+		goto('/');
+	}
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen text-center">
+	<h1 class="text-4xl font-bold mb-4">{m.notFoundTitle()}</h1>
+	<p class="text-lg mb-8">{m.notFoundText()}</p>
+	<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={goHome}>
+		{m.goToHomepageButton()}
+	</button>
+</div>
