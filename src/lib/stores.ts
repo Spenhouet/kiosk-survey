bc385439222b8a5744504855b4767e5d9f8f8c8a
Sebastian Penhouet <s.penhouet@outlook.de>
2025-05-22 23:12:52 +0200

    Start without a precreated survey

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -44,7 +44,7 @@ const createDefaultSurvey = (): Survey => {
     };
 };
 
-export const surveys = persisted<Survey[]>('surveyApp_surveys', [createDefaultSurvey()]);
+export const surveys = persisted<Survey[]>('surveyApp_surveys', []);
 
 export function createNewSurvey(): string {
     const newSurvey = createDefaultSurvey();
